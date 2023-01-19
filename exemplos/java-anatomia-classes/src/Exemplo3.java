68ce9e35919761354938f15ec7cb3ca919d1f1ec
Mich <michramalho@gmail.com>
2023-01-19 11:48:06 -0300

    adiciona exemplo 3 da aula Anatomia de Classes

---
+++ exemplos/java-anatomia-classes/src/Exemplo3.java
@@ -0,0 +1,14 @@
+public class Exemplo3 {
+    public static void main (String [] args) {
+        String primeiroNome = "Mich";
+        String segundoNome = "Ramalho";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+    
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
